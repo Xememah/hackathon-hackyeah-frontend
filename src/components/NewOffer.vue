c622f95df3cee3ffe863e4c65bf8a6aecfb2915d
Marek Kochanowski <marek@kochanow.ski>
2017-10-29 10:08:21 +0100

    Update new offer

--- src/components/NewOffer.vue
+++ src/components/NewOffer.vue
@@ -4,10 +4,11 @@
       <div class="col-sm-12 col-md-12">
         <form @submit="newOffer">
           <h1>Add a new offer</h1>
-          <input type="text" placeholder="ITEM NAME"/>
-          <input type="password"  placeholder="DESCRIPTION: QUANTITY"/>
-          <input type="text" placeholder="PICKUP ADDRESS"/>
-          <button class="login" @click="login">Log in</button>
+          <input type="text" v-model="form.title" placeholder="ITEM NAME / TITLE"/>
+          <input type="text" v-model="form.description" placeholder="DESCRIPTION: QUANTITY AND MORE"/>
+          <input type="text" v-model="form.location" placeholder="PICKUP ADDRESS"/>
+          <div class="emoji-selector">Your item's picture: <select v-model="form.picture" id="emojipicture"/></select></div>
+          <button class="login" @click="newOffer">Add a new offer</button>
         </form>
       </div>
     </div>
@@ -18,16 +19,57 @@
 export default {
   data() {
     return {
-
+      form: {
+        title: "",
+        description: "",
+        location: "",
+        picture: "🍅",
+        expiration: 1509601875,
+        pickup_start: 1509246085,
+        pickup_end: 1509560020
+      }
     }
   },
+  mounted() {
+    this.makeEmojis()
+  },
   methods: {
-    newOffer: function() {
+    makeEmojis: function() {
+      var select = document.getElementById('emojipicture')
+      var opt;
+      var range = [
+        [127813, 127854]
+      ];
+      for (var i = 0; i < range.length; i++) {
+        var range = range[i];
+        for (var x = range[0]; x < range[1]; x++) {
 
+          opt = document.createElement('option');
+          opt.value = x;
+          opt.innerHTML = "&#" + x + ";";
+          select.appendChild(opt);
+        }
+      }
+    },
+    newOffer: function() {
+      var t = 'Kraków ' + this.form.location + ', Polska';
+      var geocoder = new google.maps.Geocoder();
+      var b = this.map;
+      geocoder.geocode({ 'address': t }, (results, status) => {
+        if (status === 'OK') {
+          var res = results[0].geometry.location
+          console.log(res)        
+        }
+      })
+      console.log(this.form)
+      this.$store.newOffer(this, JSON.stringify(this.form), 'home')
     }
   }
 }
 </script>
 
 <style>
+.emoji-selector {
+  margin-top: 20px;
+}
 </style>
